6c23e2262bb3d445c25f6be789840dd0404d1b23
hidecard <hidecard1500@gmail.com>
2025-06-24 09:15:07 +0630

    Day2

---
+++ backend/models/Order.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    items: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        }
+    }],
+    total: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    shippingInfo: {
+        address :{
+            type: String,
+            required: true,
+        },
+        city :{
+            type: String,
+            required: true,
+        },
+        postalCode :{
+            type: String,
+            required: true,
+        },
+        country :{
+            type: String,
+            required: true,
+        },
+    },
+},{
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Order", orderSchema);
